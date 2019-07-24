4b83b7c3ad0c781b42b74a252a336ff0cb73044b
Harsh kumar saxena <harsh.saxena_ccv17@gla.ac.in>
2019-07-24 18:58:48 +0530

    defining draw rectangle function

--- paint.py
+++ paint.py
@@ -9,6 +9,11 @@ def draw(event):
     print(event.x,event.y)
     c.create_oval(event.x - 5,event.y -5,event.x +5,event.y +5,fill="Black")
 
+def drawRectangle():
+
+
+
+
 window=Tk()
 c=Canvas(window,width=400,height=400)
 c.pack()
