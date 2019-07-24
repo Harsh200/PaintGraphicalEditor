e1de61b720676aa15b81e35486f0f4374c9c5c51
Harsh kumar saxena <harsh.saxena_ccv17@gla.ac.in>
2019-07-24 18:54:10 +0530

    passing event as argument

--- paint.py
+++ paint.py
@@ -5,7 +5,7 @@ def callback(event):
     print(event.x,event.y)
     c.create_oval(event.x -5,event.y -5, event.x +5,event.y +5,fill="Green")
 
-def draw():
+def draw(event):
 
 window=Tk()
 c=Canvas(window,width=400,height=400)
