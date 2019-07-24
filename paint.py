aa84933a9b7a6b9925b34a7918c934938b0704fa
Harsh kumar saxena <harsh.saxena_ccv17@gla.ac.in>
2019-07-24 19:07:20 +0530

    defining coordinate

--- paint.py
+++ paint.py
@@ -1,15 +1,25 @@
 from tkinter import *
 
-
+Draw=False
+x1=0
+y1=0
 def callback(event):
     print(event.x,event.y)
     c.create_oval(event.x -5,event.y -5, event.x +5,event.y +5,fill="Green")
 
-def draw(event):
+def Draw(event):
     print(event.x,event.y)
     c.create_oval(event.x - 5,event.y -5,event.x +5,event.y +5,fill="Black")
 
 def drawRectangle(event):
+    global Draw,x1,y1
+    if Draw:
+        c.create_rectangle(x1,y1,event.x,event.y,fill="Red")
+        Draw=False
+    else:
+        x1=event.x
+        y1=event.y
+        Draw=True
 
 
 
@@ -18,8 +28,8 @@ window=Tk()
 c=Canvas(window,width=400,height=400)
 c.pack()
 c.bind("<Button-1>",callback)
-c.bind("<B1-Motion>",draw)
-c.bind("<b3-Motion>",drawRectangle)
+c.bind("<B1-Motion>",Draw)
+c.bind("<Button-3>",drawRectangle)
 
 
 
