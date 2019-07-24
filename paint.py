add31d9864afb9be321d89ffad1715879b0c36ee
Harsh kumar saxena <harsh.saxena_ccv17@gla.ac.in>
2019-07-24 18:53:45 +0530

    defining the draw function

--- paint.py
+++ paint.py
@@ -5,6 +5,7 @@ def callback(event):
     print(event.x,event.y)
     c.create_oval(event.x -5,event.y -5, event.x +5,event.y +5,fill="Green")
 
+def draw():
 
 window=Tk()
 c=Canvas(window,width=400,height=400)
